d4d8b13f06ab1cf47d3f758546e233f9869e939a
Golubev Vladimir <goso@list.ru>
2021-07-27 12:35:12 +0300

    t2

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ AQA-19
 # Домашнее задание к занятию 2.2 Selenide
 
 Задача №1 - Заказ доставки карты  
+Задача №2 - Взаимодействие с комплексными элементами (необязательная)  
 
 [![Build status](https://ci.appveyor.com/api/projects/status/lo0fjp0dyshvb7ae?svg=true)](https://ci.appveyor.com/project/goso-nct/netology-auto-dz2-2)
--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,30 +1,41 @@
 package ru.netology;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.util.Calendar;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardDeliveryTest {
 
-    @Test
-    void shouldWorking() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_MONTH, 5);
-        String date = new SimpleDateFormat("dd.MM.yyyy").format(calendar.getTime());
-
+    @BeforeEach
+    public void setUp() {
         open("http://localhost:9999");
+    }
 
+    @AfterEach
+    void tearDown() {
+        closeWebDriver();
+    }
+
+    @Test
+    void shouldWorkingT1() {
         $("[data-test-id=city] input").setValue("Уфа");
+
+        String date = LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $("[data-test-id=date] input").doubleClick();
         $("[data-test-id=date] input").sendKeys(Keys.DELETE);
         $("[data-test-id=date] input").setValue(date);
+
         $("[data-test-id=name] input").setValue("Иванов");
         $("[data-test-id=phone] input").setValue("+71234567890");
         $("[data-test-id=agreement] .checkbox__box").click();
@@ -35,4 +46,31 @@ public class CardDeliveryTest {
                 .shouldHave(exactText("Встреча успешно забронирована на " + date));
     }
 
+    @Test
+    void shouldWorkingT2() {
+        $("[data-test-id=city] input").setValue("мо");
+        $$("[class=popup__content] .menu-item__control").get(2).click();
+        assertEquals("Москва", $("[data-test-id=city] input").getValue());
+
+        LocalDate today = LocalDate.now();
+        LocalDate needDate = today.plusDays(7);
+        int month = needDate.getMonthValue() - today.getMonthValue();
+
+        $("[data-test-id=date] input").click();
+        if (month == 1) {
+            $$(".calendar__arrow_direction_right").get(1).click();
+        }
+        $(byText(String.valueOf(needDate.getDayOfMonth()))).shouldHave(cssClass("calendar__day")).click();
+
+        $("[data-test-id=name] input").setValue("Иванов");
+        $("[data-test-id=phone] input").setValue("+71234567890");
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $("[role=button] .button__content").click();
+
+        $("[data-test-id=notification] .notification__content")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(exactText("Встреча успешно забронирована на "
+                        + needDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))));
+
+    }
 }
